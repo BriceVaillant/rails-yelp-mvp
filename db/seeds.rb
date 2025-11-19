e33bd4566e6367b6a454aeee73e877d12ecb2b9e
bricevaillant <naw.brice@gmail.com>
2025-11-19 14:02:48 +0100

    problem solved

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+# 1. Clean the database 🗑️
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+# 2. Create the instances 🏗️
+puts "Creating restaurants..."
+Restaurant.create!(name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "0890876548", category: "chinese")
+puts "Created Dishoom"
+Restaurant.create!(name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "0890234548", category: "italian")
+puts "Created Pizza East"
+Restaurant.create!(name: "cacahuete", address: "7 rue france", phone_number: "0843546548", category: "french")
+puts "Created cacahuete"
+Restaurant.create!(name: "Frite alors", address: " 143 belgique E1 6PQ", phone_number: "089082348", category: "belgian")
+puts "Created Frite alors"
+Restaurant.create!(name: "myrtillo", address: "12343 rue des trois quarts france", phone_number: "0812376548", category: "french")
+puts "Created myrtillo"
+Restaurant.create!(name: "nipon gurber", address: "45 rue tokyo, japan", phone_number: "0890234354", category: "japanese")
+puts "Created nipon gurber"
+
+# 3. Display a message 🎉
+puts "Finished! Created #{Restaurant.count} restaurants."
